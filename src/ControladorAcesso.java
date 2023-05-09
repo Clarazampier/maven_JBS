a6b0e213ec50c30f62e722234680e95280650ce1
Ana Clara <clarazampiert@gmail.com>
2023-05-09 17:37:20 -0300

    quarto commit

--- src/ControladorAcesso.java
+++ src/ControladorAcesso.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ControladorAcesso {
     private List<String> codigosRegistrados;
@@ -10,13 +11,30 @@ public class ControladorAcesso {
         codigosRegistrados = new ArrayList<>();
         datasEntrada = new ArrayList<>();
     }
+
+    public void iniciarAcesso() {
+        Scanner scanner = new Scanner(System.in);
+        String codigo;
+
+        do {
+            System.out.print("Digite o código do crachá (ou 0 para sair): ");
+            codigo = scanner.nextLine();
+
+            if (!codigo.equals("0")) {
+                acessar(codigo);
+            }
+        } while (!codigo.equals("0"));
+    }
+
     public void acessar(String codigo) {
         if (!codigosRegistrados.contains(codigo)) {
             codigosRegistrados.add(codigo);
             datasEntrada.add(LocalDateTime.now());
             System.out.println("Bem-vindo, acesso liberado");
+            registrarSaida(codigo);
         } else {
             System.out.println("Obrigado pela visita");
+
         }
     }
 
