2b1236c9e48ff96ac380a6616fde0dcd38cbad73
Ana Clara <clarazampiert@gmail.com>
2023-05-09 17:09:54 -0300

    terceiro commit

--- src/Teste.java
+++ src/Teste.java
@@ -1,2 +1,14 @@
 public class Teste {
+    public static void main(String[] args) {
+        ControladorAcesso controlador = new ControladorAcesso();
+
+        //teste 1: acesso liberado
+        controlador.acessar("1234");
+        //teste 2: visita repetida
+        controlador.acessar("1234");
+        //teste 3: acesso liberado para outro código
+        controlador.acessar("5678");
+        //teste 4: registro de saída
+        controlador.registrarSaida("1234");
+    }
 }
